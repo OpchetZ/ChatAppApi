ed41ae0df8463fc0f4e3e6c7be83db699db1f53f
OpchetZ <varanyookasamsuk@gmail.com>
2026-04-10 14:01:43 +0700

    MessageRepo

--- api/Repository/MessageRepository.cs
+++ api/Repository/MessageRepository.cs
@@ -6,6 +6,7 @@ using api.Data;
 using api.Helpers;
 using api.interfaces;
 using api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
 {
@@ -16,29 +17,52 @@ namespace api.Repository
         {
             _context = context;
         }
-        public Task<Massages> CreateAsync(Massages massagesModel)
+        public async Task<Massages> CreateAsync(Massages massagesModel)
         {
-            throw new NotImplementedException();
+            await _context.Massages.AddAsync(massagesModel);
+            await _context.SaveChangesAsync();
+            return massagesModel;
         }
 
-        public Task<Massages?> DeleteAsync(int id)
+        public async Task<Massages?> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var messagemodel = await _context.Massages.FirstOrDefaultAsync(x => x.Id ==id);
+            if (messagemodel == null)
+            {
+                return null;
+            }
+            _context.Massages.Remove(messagemodel);
+            await _context.SaveChangesAsync();
+            return messagemodel;
         }
 
-        public Task<List<Massages>> GetAllAsync(MessageQuery messageQuery)
+        public async Task<List<Massages>> GetAllAsync(MessageQuery messageQuery)
         {
-            throw new NotImplementedException();
+            var message = _context.Massages.Include(a => a.AppUser).AsQueryable();
+            if (!string.IsNullOrWhiteSpace(messageQuery.keyword))
+            {
+                message = message.Where(s => s.Content == messageQuery.keyword);
+            }
+            return await message.ToListAsync();
         }
 
-        public Task<Massages?> GetByIdAsync(int id)
+        public async Task<Massages?> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Massages.Include(a => a.AppUser).FirstOrDefaultAsync(c => c.Id ==id);
         }
 
-        public Task<Massages?> UpdateAsync(int id, Massages massagesModel)
+        public async Task<Massages?> UpdateAsync(int id, Massages massagesModel)
         {
-            throw new NotImplementedException();
+            var checkmessage = await _context.Massages.FindAsync(id);
+            if (checkmessage == null)
+            {
+                return null;
+            }
+
+            checkmessage.Content = massagesModel.Content;
+            await _context.SaveChangesAsync();
+
+            return checkmessage;
         }
     }
 }
